1cdb2a12a07556cb04d1ac3997aa1754d809a66a
JoannaTabbi <joanna.tabbi@gmail.com>
2022-03-21 16:43:38 +0100

    get one product

---
+++ front/js/product.js
@@ -0,0 +1,25 @@
+const urlSearchParams = new URLSearchParams(window.location.search);
+const productId = urlSearchParams.get("id");
+
+fetch(`http://localhost:3000/api/products/${productId}`)
+  .then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then((product) => {
+   document.querySelector('.item__img').innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}">`;
+   document.getElementById('title').innerText = `${product.name}`;
+   document.getElementById('price').innerText = `${product.price}`;
+   document.getElementById('description').innerText = `${product.description}`;
+   
+   product.colors.forEach(color => {
+    const colorOption = document.createElement("option");
+    colorOption.setAttribute('value', `${color}`);
+    colorOption.innerText = `${color}`;
+    document.getElementById('colors').appendChild(colorOption);
+    });
+  })
+  .catch((err) => {
+    alert(`Une erreur est survenue: ${err}`);
+  });
