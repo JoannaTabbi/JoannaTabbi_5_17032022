47b38eab9e64893e5d72848fc7bdc610ac55362e
JoannaTabbi <joanna.tabbi@gmail.com>
2022-03-21 18:04:51 +0100

    format documents with Prettier

--- front/js/product.js
+++ front/js/product.js
@@ -8,20 +8,24 @@ fetch(`http://localhost:3000/api/products/${productId}`)
     }
   })
   .then((product) => {
-   document.querySelector('head > title').innerText = `${product.name}`;   
-   document.querySelector('.item__img').innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}">`;
-   document.getElementById('title').innerText = `${product.name}`;
-   document.getElementById('price').innerText = `${product.price}`;
-   document.getElementById('description').innerText = `${product.description}`;
-   
-   product.colors.forEach(color => {
-    const colorOption = document.createElement("option");
-    colorOption.setAttribute('value', `${color}`);
-    colorOption.innerText = `${color}`;
-    document.getElementById('colors').appendChild(colorOption);
+    document.querySelector("head > title").innerText = `${product.name}`;
+    document.querySelector(
+      ".item__img"
+    ).innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}">`;
+    document.getElementById("title").innerText = `${product.name}`;
+    document.getElementById("price").innerText = `${product.price}`;
+    document.getElementById("description").innerText = `${product.description}`;
+
+    product.colors.forEach((color) => {
+      const colorOption = document.createElement("option");
+      colorOption.setAttribute("value", `${color}`);
+      colorOption.innerText = `${color}`;
+      document.getElementById("colors").appendChild(colorOption);
     });
-    document.getElementById('quantity').setAttribute('value', '1');
+
+    document.getElementById("quantity").setAttribute("value", "1");
   })
+  
   .catch((err) => {
     alert(`Une erreur est survenue: ${err}`);
   });
--- front/js/script.js
+++ front/js/script.js
@@ -5,10 +5,10 @@ fetch("http://localhost:3000/api/products")
     }
   })
   .then((products) => {
-      let html = "";
-    products.forEach(product => {
-        console.log(product);
-        html+=` <a href="./product.html?id=${product._id}">
+    let html = "";
+    products.forEach((product) => {
+      console.log(product);
+      html += ` <a href="./product.html?id=${product._id}">
         <article>
           <img src="${product.imageUrl}" alt="${product.altTxt}">
           <h3 class="productName">${product.name}</h3>
@@ -16,10 +16,10 @@ fetch("http://localhost:3000/api/products")
         </article>
       </a>`;
     });
-    const items = document.getElementById('items');
+    const items = document.getElementById("items");
     items.innerHTML = html;
   })
   .catch((err) => {
-    const items = document.getElementById('items');
+    const items = document.getElementById("items");
     items.innerHTML = `Une erreur est survenue: ${err}`;
   });
