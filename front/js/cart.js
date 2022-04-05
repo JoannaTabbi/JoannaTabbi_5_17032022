75369d4cf307d38e62996cba5a5f1d63c2f76d72
JoannaTabbi <joanna.tabbi@gmail.com>
2022-04-05 11:49:38 +0200

    delete productFromCart, refactor code

--- front/js/cart.js
+++ front/js/cart.js
@@ -9,7 +9,6 @@ let totalProductQuantity = 0;
 
 cartItems.forEach((cartItem) => {
   const productId = cartItem._id;
-  console.log(productId);
 
   //gets the product information from API
 
@@ -20,25 +19,16 @@ cartItems.forEach((cartItem) => {
       }
     })
     .then((product) => {
-      // reconstructs cart product with name, image and price from API
-
-      let productFromCart = {
-        _id: product._id,
-        name: product.name,
-        color: cartItem.color,
-        imageUrl: product.imageUrl,
-        altTxt: product.altTxt,
-        price: product.price,
-        quantity: cartItem.quantity,
-        totalPrice: function () {
-          let x = this.quantity * this.price;
-          return x;
-        },
-      };
-
-      CartDetails(productFromCart);
-      totalQuantity(productFromCart);
-      totalPrice(productFromCart);
+      document.getElementById("cart__items").innerHTML = CartDetails(
+        product,
+        cartItem
+      );
+      document.getElementById("totalQuantity").innerText =
+        totalQuantity(cartItem);
+      document.getElementById("totalPrice").innerText = totalPrice(
+        product,
+        cartItem
+      );
     })
 
     .catch((err) => {
@@ -49,21 +39,21 @@ cartItems.forEach((cartItem) => {
 
 // displays the details of each cart item
 
-let CartDetails = (product) => {
-  const itemCard = ` <article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
+let CartDetails = (apiData, cartData) => {
+  cartItemsHtml += `<article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
     <div class="cart__item__img">
-      <img src="${product.imageUrl}" alt="${product.altTxt}">
+      <img src="${apiData.imageUrl}" alt="${apiData.altTxt}">
     </div>
     <div class="cart__item__content">
       <div class="cart__item__content__description">
-        <h2>${product.name}</h2>
-        <p>${product.color}</p>
-        <p>${product.price} €</p>
+        <h2>${apiData.name}</h2>
+        <p>${cartData.color}</p>
+        <p>${apiData.price} €</p>
       </div>
       <div class="cart__item__content__settings">
         <div class="cart__item__content__settings__quantity">
           <p>Qté : </p>
-          <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.quantity}">
+          <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${cartData.quantity}">
         </div>
         <div class="cart__item__content__settings__delete">
           <p class="deleteItem">Supprimer</p>
@@ -71,22 +61,19 @@ let CartDetails = (product) => {
       </div>
     </div>
   </article>`;
-
-  cartItemsHtml += itemCard;
-
-  document.getElementById("cart__items").innerHTML = cartItemsHtml;
+  return cartItemsHtml;
 };
 
 // displays total quantity
 
-let totalQuantity = (product) => {
-  document.getElementById("totalQuantity").innerText = totalProductQuantity +=
-    product.quantity;
+let totalQuantity = (cartData) => {
+  totalProductQuantity += cartData.quantity;
+  return totalProductQuantity;
 };
 
 // display total product price
 
-let totalPrice = (product) => {
-  document.getElementById("totalPrice").innerText = totalProductPrice +=
-    product.totalPrice();
+let totalPrice = (apiData, cartData) => {
+  totalProductPrice += apiData.price * cartData.quantity;
+  return totalProductPrice;
 };
